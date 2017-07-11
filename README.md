cad3166514bac44db3974fc08f9342e4c17dd313
TadaMatz <matz@west.sd.keio.ac.jp>
2017-07-11 15:42:59 +0900

    responsive canvas size on html

--- README.md
+++ README.md
@@ -1,9 +1,12 @@
 #OooWaterMark
-メモ
+中身はJavaScript, HTML, CSSの練習  
+特にp5.jsとか使っちゃう系
+一応電子透かしのデモに使用予定
 
 * 複数キャンバスを使うなら[instance mode](https://github.com/processing/p5.js/wiki/p5.js-overview#instantiation--namespace)を使う
 	* これも役に立つ <http://www.joemckaystudio.com/multisketches/> 
-* 
+* ```<div>```はブロック要素（一般的に前後に改行が入る）として，```<span>```はインライン要素（一般的に前後に改行が入らない）として使われる
+* [p5.dom library](https://github.com/processing/p5.js/wiki/Beyond-the-canvas)はなんだかんだ重要
 
 <a href="http://www.youtube.com/watch?feature=player_embedded&v=YOUTUBE_MOVIE_ID_HERE
 " target="_blank"><img src="http://img.youtube.com/vi/YOUTUBE_MOVIE_ID_HERE/0.jpg" 
--- sketch.js
+++ sketch.js
@@ -1,18 +1,28 @@
+//canvas 1
 var p5_1_f = function(p){
   p.setup = function(){
-    p.createCanvas(640, 480);
+    p.createCanvas(
+      document.getElementById("p5canvas_1").getAttribute("width"), 
+      document.getElementById("p5canvas_1").getAttribute("height")
+      );
   };
-
   p.draw = function(){
+    p.stroke(0);
+    p.fill(255);
+    p.rect(0, 0, p.width, p.height);
     p.fill(0);
     p.ellipse(50, 50, 80, 80);
   };
 };
 var p5_1 = new p5(p5_1_f, 'p5canvas_1');
 
+//canvas 2
 var p5_2_f = function(p){
   p.setup = function(){
-    p.createCanvas(640, 480);
+    p.createCanvas(
+      document.getElementById("p5canvas_1").getAttribute("width"), 
+      document.getElementById("p5canvas_1").getAttribute("height")
+      );
   };
 
   p.draw = function(){
