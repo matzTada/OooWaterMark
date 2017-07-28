329484a95828ad6962c2bf3277aa4b8875679080
matzTada <matz@west.sd.keio.ac.jp>
2017-07-28 17:01:23 +0900

    add start shell script

---
+++ WaterMarkGUI/start.sh
@@ -0,0 +1,13 @@
+if [ -z "${PORT+x}" ] ; then #if PORT is not defined, define.
+  echo "PORT was not defined before, define PORT"
+  export PORT=3000 #change here if you start express app without Docker
+fi
+echo "PORT: $PORT"
+
+if [ -z "${JSON_FILE_DIR+x}" ] ; then #if JSON_FILE_DIR is not defined, define.
+  echo "JSON_FILE_DIR was not defined before, define JSON_FILE_DIR"
+  export JSON_FILE_DIR="./public/jsons" #change here if you start express app without Docker
+fi
+echo "JSON_FILE_DIR: $JSON_FILE_DIR"
+
+npm start
