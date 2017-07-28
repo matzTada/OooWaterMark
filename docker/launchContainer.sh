ce201d960c68c8bc870f084a6ebe503f9f7ad099
Tada Matz <matz@west.sd.keio.ac.jp>
2017-07-28 19:47:05 +0900

    modify port fowarding setting of docker

--- docker/launchContainer.sh
+++ docker/launchContainer.sh
@@ -6,5 +6,5 @@ GUI_PORT=3330
 # JSON_DIR="../config_jsons" #relative path allowed. change here for config json files
 JSON_DIR="../WaterMarkGUI/public/jsons" #relative path allowed. change here for config json files
 JSON_DIR=$(cd $(dirname $0) && cd $JSON_DIR && pwd) #convert to absolute path
-sudo docker run -it -v $JSON_DIR:/jsons -p $GUI_PORT:3333 -p 443:443 --rm watermarkgui /bin/bash ./start.sh
+sudo docker run -it -v $JSON_DIR:/jsons -p $GUI_PORT:3333 --rm watermarkgui /bin/bash ./start.sh
 
